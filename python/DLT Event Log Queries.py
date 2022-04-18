0bf8fd4acea1d1a7baa11d1b1ae4a4e7dbe090ef
ryoma-nagata <ryoma-nagata>
2022-04-18 23:24:36 +0000

    revision test

--- python/DLT Event Log Queries.py
+++ python/DLT Event Log Queries.py
@@ -7,7 +7,7 @@
 # Fill in the pipelines_id, pipeline_name and storage_location
 pipelines_id = "" #used to find the event path if storage location is blank
 pipeline_name = "" 
-storage_location = "" #may be blank, in which case, the pipelines_id is used for the event path
+storage_location = "" #may be blank, in which case, the pipelines_id is used for the event path tets
 
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
